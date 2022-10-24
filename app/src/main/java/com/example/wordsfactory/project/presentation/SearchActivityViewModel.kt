a71df3159e3544c01fff9fe2d71b8118e91568cf
MaksimAkimov2003 <101564073+MaksimAkimov2003@users.noreply.github.com>
2022-10-25 01:07:20 +0700

    delete useless imports

--- app/src/main/java/com/example/wordsfactory/project/presentation/SearchActivityViewModel.kt
+++ app/src/main/java/com/example/wordsfactory/project/presentation/SearchActivityViewModel.kt
@@ -1,21 +1,12 @@
 package com.example.wordsfactory.project.presentation
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.wordsfactory.project.domain.UseCases.SearchWordUseCase
-import com.example.wordsfactory.project.network.BASE_URL
-import com.example.wordsfactory.project.network.SearchWordsService
 import com.example.wordsfactory.project.network.WordInformation
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
 import kotlinx.coroutines.*
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
 
 class SearchActivityViewModel(
     private val searchWordUseCase: SearchWordUseCase,
